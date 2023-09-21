df82ea8aa233cf63a612a4f08ff7ce186224f516
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-21 01:42:20 +0100

    Feat: Added event deletion endpoint

--- src/controller/eventController.js
+++ src/controller/eventController.js
@@ -1,26 +1,26 @@
-const Event = require('../models/events');
+const Event = require("../models/events");
 
-exports.getEventDyId = async(req,res) =>{
-    // Retrieve the event by its ID
-    const eventId = req.params.id;
+exports.getEventDyId = async (req, res) => {
+  // Retrieve the event by its ID
+  const eventId = req.params.id;
 
-    try {
-        const event =await Event.findOne({
-            where: {
-                id:eventId
-            }
-        });
-        if (!event){
-            res.status(404).json({error: "Event not Found"});
-        }
-         // Send the event as the response
-        res.status(200).json(event);
-    } catch (error) {
-        // Handle any errors that occur during the process
-    console.error('Error retrieving event by ID:', error);
-    res.status(500).json({ error: 'Internal server error' });   
+  try {
+    const event = await Event.findOne({
+      where: {
+        id: eventId,
+      },
+    });
+    if (!event) {
+      res.status(404).json({ error: "Event not Found" });
     }
-}
+    // Send the event as the response
+    res.status(200).json(event);
+  } catch (error) {
+    // Handle any errors that occur during the process
+    console.error("Error retrieving event by ID:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 exports.updateEvent = async (req, res) => {
   const eventId = req.params.eventId;
@@ -39,7 +39,7 @@ exports.updateEvent = async (req, res) => {
   const existingEvent = await Event.findByPk(eventId);
 
   if (!existingEvent) {
-    return res.status(404).json({ error: 'Event not found' });
+    return res.status(404).json({ error: "Event not found" });
   }
 
   // Update event details using Sequelize
@@ -54,5 +54,25 @@ exports.updateEvent = async (req, res) => {
 
   await existingEvent.save();
 
-  res.status(200).json({ message: 'Event updated successfully' });
+  res.status(200).json({ message: "Event updated successfully" });
+};
+
+exports.deleteEvent = async (req, res) => {
+  const eventId = req.params.eventId;
+
+  try {
+    // Check if the event exists
+    const existingEvent = await Event.findByPk(eventId);
+
+    if (!existingEvent) {
+      return res.status(404).json({ error: "Event not found" });
+    }
+
+    //delete the event
+    await existingEvent.destroy();
+    return res.status(200).json({ message: "Event deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting event by ID:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
